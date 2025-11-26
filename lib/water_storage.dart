2788ffee48de3ab75aefedcb641be3ce2cbbd7a7
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-11-26 19:55:18 +0300

    Add WaterStorage class with async file handling (using await for reading JSON)

--- lib/water_storage.dart
+++ lib/water_storage.dart
@@ -13,3 +13,32 @@ class WaterData {
     return {'total_glasses': totalGlasses};
   }
 }
+
+class WaterStorage {
+  Future<File> getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/water_data.json';
+    return File(path);
+  }
+
+  Future<WaterData> loadWaterData() async {
+    final file = await getFile();
+
+    if (!await file.exists()) {
+      return WaterData(totalGlasses: 0);
+    }
+
+    final content = await file.readAsString();
+    final dataMap = jsonDecode(content);
+    return WaterData.fromJson(dataMap);
+  }
+
+  Future<void> saveWaterData(WaterData data) async {
+    final file = await getFile(); //dosyayı al
+    final dataMap = data.toJson(); //WaterData -> Map
+    final jsonString = jsonEncode(dataMap); //Map ->JSON string
+    await file.writeAsString(jsonString); //Dosyaya yaz
+  }
+
+  // Note: getFile() and loadWaterData() use async/await because file operations are asynchronous.
+}
