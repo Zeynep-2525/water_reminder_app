4e35868c60e608461df56d7f5de78a08c8e7587e
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-11-26 20:48:35 +0300

    Create WaterHomePage with UI, state management, and add glass functionality

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../models/water_data.dart';
+import '../services/water_storage.dart';
+
+class WaterHomePage extends StatefulWidget {
+  @override
+  _WaterHomePageState createState() => _WaterHomePageState();
+}
+
+class _WaterHomePageState extends State<WaterHomePage> {
+  WaterData _waterData = WaterData(totalGlasses: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _loadData() async {
+    WaterData data = await storage.loadWaterData();
+    setState(() {
+      _waterData = data;
+    });
+  }
+
+  void _addGlass() async {
+    setState(() {
+      _waterData.totalGlasses += 1;
+    });
+    await storage.saveWaterData(_waterData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Water Reminder')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Bardak Sayısı: ${_waterData.totalGlasses}'),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _addGlass, child: Text('Bardak Ekle')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final storage = WaterStorage();
